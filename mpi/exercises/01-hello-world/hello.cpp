df24de9432c49cb03f36524c1f96b40d0cd46ca7
Nils Horneff <nils.horneff@outlook.de>
2026-06-24 11:07:32 +0300

    mpi exercise 1

--- mpi/exercises/01-hello-world/hello.cpp
+++ mpi/exercises/01-hello-world/hello.cpp
@@ -3,9 +3,20 @@
 // SPDX-License-Identifier: MIT
 
 #include <iostream>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    // TODO: say hello! in parallel
-    std::cout << "Hello!" << std::endl;
+    MPI_Init(&argc, &argv);
+    int rank, numberOfRanks;
+    MPI_Comm_size(MPI_COMM_WORLD, &numberOfRanks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank==0){
+        std::cout << rank << std::endl;
+    } else if (rank==numberOfRanks-1){
+        std::cout << "I'm the last but not least" << std::endl;
+    } else if (rank==42){
+        std::cout << "I'm the Answer to the Ultimate Question of Life, the Universe, and Everything!" << std::endl;
+    }
+    MPI_Finalize();
 }
